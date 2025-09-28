916a73a6fd059789e73c8746aadc43048a9d2a13
Andrew Valenzuela <andrewvalenzuela@Andrews-Mac-mini.local>
2025-09-28 08:27:19 -0700

    Capture line number from user input

--- phone_formatter.py
+++ phone_formatter.py
@@ -4,5 +4,9 @@ CSC 101
 09/28/2025
 Using the % and // operators to format a 10-digit phone number
 """
+#asking users for their phone number
+phone_number = int(input("Please enter your 10-digit phone number: "))
 
-phone_number = int(input("Please enter your 10-digit phone number: ")) #asking the user for their phone number
+#gathering last 4 digits first 
+line_number = phone_number % 1000
+phone_number //= 10000 
