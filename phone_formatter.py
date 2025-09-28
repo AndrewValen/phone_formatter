9808efc47d70e36f7001611be817af5e46378b78
Andrew Valenzuela <andrewvalenzuela@Andrews-Mac-mini.local>
2025-09-28 08:23:07 -0700

    Add phone number prompt

--- phone_formatter.py
+++ phone_formatter.py
@@ -4,3 +4,5 @@ CSC 101
 09/28/2025
 Using the % and // operators to format a 10-digit phone number
 """
+
+phone_number = int(input("Please enter your 10-digit phone number: ")) #asking the user for their phone number
